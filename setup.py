10d29bc346b8c2c2fd9abadba549491b98b75473
Douglas Soares de Andrade <contato@douglasandrade.com>
2016-05-04 01:43:42 -0300

    An initial setup.py to distribute and test this code

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from toxiproxy import VERSION
+
+NAME = 'toxiproxy'
+DESCRIPTION = 'Python library for Toxiproxy'
+LONG_DESCRIPTION = """\
+A Python library for controlling Toxiproxy. Can be used in resiliency testing."""
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author='Douglas Soares de Andrade',
+      author_email='contato@douglasandrade.com',
+      url='https://github.com/douglas/toxiproxy-python',
+      packages=['toxiproxy'],
+      scripts=[],
+      license='MIT License',
+      install_requires=[
+          'six',
+      ],
+      test_suite='test',
+      tests_require=[
+      ],
+      platforms='Any',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Software Development',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Software Development :: Libraries :: Python Modules']
+      )
