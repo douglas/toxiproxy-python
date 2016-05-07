0bdc2e859cf9d123ec1727da50d3d10fa67f0d1b
Douglas Soares de Andrade <contato@douglasandrade.com>
2016-05-07 18:41:12 -0300

    Removing the comment break

--- toxiproxy/exceptions.py
+++ toxiproxy/exceptions.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
-# Custom exceptions to match the exceptions used
-# in the Ruby wrapper
+# Custom exceptions to match the exceptions used in the Ruby wrapper
 ProxyExists = type("ProxyExists", (Exception,), {})
 NotFound = type("NotFound", (Exception,), {})
 InvalidToxic = type("InvalidToxic", (Exception,), {})
