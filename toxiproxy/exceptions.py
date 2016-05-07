5ed8d5856ccb7ef986c73b06e0be74b0fd2e1e61
Douglas Soares de Andrade <contato@douglasandrade.com>
2016-05-07 00:17:30 -0300

    Adding the exceptions to match the Ruby wrapper
    I know I could maintain these exceptions on the api.py file, but I
    wanted that the traceback show that they came from the exceptions
    module.
    
    Also a bit of metaclassing to avoid the need to create a complete class
    definition.

---
+++ toxiproxy/exceptions.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# Custom exceptions to match the exceptions used
+# in the Ruby wrapper
+ProxyExists = type("ProxyExists", (Exception,), {})
+NotFound = type("NotFound", (Exception,), {})
+InvalidToxic = type("InvalidToxic", (Exception,), {})
