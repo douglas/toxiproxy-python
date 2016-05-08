943dd19e3757edb570a573b7232615320589eed0
Douglas Soares de Andrade <contato@douglasandrade.com>
2016-05-08 16:36:10 -0300

    Refactor the code and new methods
    - Refactor the code to give the right object the right responsabilities
    - the Proxy object should know how to destroy itself and how to be
    enabled and disabled.
    - Add the methods disable and enable to the Proxy object
    - Add the method destroy to the Toxiproxy object to be able to create
    and destroy proxies.

--- toxiproxy/__init__.py
+++ toxiproxy/__init__.py
@@ -2,9 +2,57 @@
 
 import socket
 
-from collections import namedtuple
 from contextlib import closing
 from .api import Intoxicated
+
+
+class Proxy(object):
+    """ Represents a Proxy object """
+
+    def __init__(self, **kwargs):
+        self.name = kwargs["name"]
+        self.upstream = kwargs["upstream"]
+        self.api_server = kwargs["api_server"]
+        self.enabled = kwargs["enabled"]
+        self.listen = kwargs["listen"]
+
+    def destroy(self):
+        """ Destroy a Toxiproxy proxy """
+
+        return bool(self.api_server.delete("/proxies/%s" % self.name))
+
+    def disable(self):
+        """
+        Disables a Toxiproxy - this will drop all active connections and
+        stop the proxy from listening.
+        """
+
+        return self.__enable_proxy(False)
+
+    def enable(self):
+        """
+        Enables a Toxiproxy - this will cause the proxy to start listening again.
+        """
+
+        return self.__enable_proxy(True)
+
+    def __enable_proxy(self, enabled=False):
+        """ Enables or Disable a proxy """
+
+        # Lets build a dictionary to send the data to the Toxiproxy server
+        json = {
+            "enabled": enabled,
+        }
+
+        proxy_info = self.api_server.post("/proxies/%s" % self.name, json=json).json()
+        self.__update_proxy(**proxy_info)
+
+        return True
+
+    def __update_proxy(self, **proxy_info):
+        """ Update proxy information after some action """
+
+        self.__dict__.update(**proxy_info)
 
 
 class Toxiproxy(object):
@@ -52,9 +100,9 @@ class Toxiproxy(object):
             json["enabled"] = enabled
 
         proxy_info = self.api_server.post("/proxies", json=json).json()
+        proxy_info["api_server"] = self.api_server
 
         # Lets create a Proxy object to hold all its data
-        Proxy = namedtuple("Proxy", ["upstream", "name", "listen", "enabled", "toxics"])
         proxy = Proxy(**proxy_info)
 
         # Add the new proxy to the toxiproxy proxies collection
@@ -63,6 +111,10 @@ class Toxiproxy(object):
         return proxy
 
     def destroy(self, proxy):
-        """ Destroy a Toxiproxy proxy """
+        """ Delete a toxiproxy proxy """
 
-        return bool(self.api_server.delete("/proxies/%s" % proxy.name))
+        if proxy.destroy() is True:
+            self.proxies.remove(proxy)
+            return True
+        else:
+            return False
