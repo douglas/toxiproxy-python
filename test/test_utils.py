3e695284fc75775fbdbdf1ccb96e75a5877ff7c9
Douglas Soares de Andrade <contato@douglasandrade.com>
2017-02-28 19:37:34 -0300

    Creating
    Moving the test tcp_server and connect_to_proxy functions to the
    test_utils.py file as they are only used during tests.

---
+++ test/test_utils.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+import socketserver
+
+from contextlib import closing, contextmanager
+from builtins import bytes
+
+
+class TCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("%s wrote: %s" % (self.client_address[0], self.data))
+        if self.data == bytes(b"omg"):
+            self.request.sendall(bytes(b"omgs"))
+
+
+@contextmanager
+def tcp_server():
+    server = socketserver.TCPServer(("127.0.0.1", 0), RequestHandlerClass=TCPHandler)
+
+    thread = threading.Thread(target=server.serve_forever, kwargs={"poll_interval": 0.01})
+    thread.daemon = True
+
+    try:
+        thread.start()
+        yield server
+    finally:
+        server.shutdown()
+
+
+def connect_to_proxy(proxy):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        host, port = proxy.listen.split(":")
+        sock.connect((host, int(port)))
+        sock.sendall(bytes(b"omg"))
+        response = sock.recv(1024)
+        print("Received: {}".format(response))
